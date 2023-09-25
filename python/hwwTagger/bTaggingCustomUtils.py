51727e28df8eb68cb532a32c99a24e10a6415200
StephenChao <yuzhe.zhao@cern.ch>
2023-09-25 05:43:24 -0500

    Add HWW taggers

---
+++ python/hwwTagger/bTaggingCustomUtils.py
@@ -0,0 +1,24 @@
+def updateSupportedBtagDiscr(supportedBtagInfos, supportedBtagDiscr, supportedMetaDiscr):
+    
+    ## Update taggers in DeepHWWV1, InclParticleTransformerV1
+    from PhysicsTools.PFNano.hwwTagger.pfMassDecorrelatedDeepHWWV1_cff import _pfMassDecorrelatedDeepHWWV1JetTagsProbs, _pfMassDecorrelatedDeepHWWV1JetTagsMetaDiscrs
+    from PhysicsTools.PFNano.hwwTagger.pfMassDecorrelatedInclParticleTransformerV1_cff import _pfMassDecorrelatedInclParticleTransformerV1JetTagsProbs, _pfMassDecorrelatedInclParticleTransformerV1JetTagsMetaDiscrs
+    
+    # update supportedBtagDiscr
+    supportedBtagInfos.extend(["pfMassDecorrelatedDeepHWWV1TagInfos"])
+    supportedBtagInfos.extend(["pfMassDecorrelatedInclParticleTransformerV1TagInfos"])
+    for disc in _pfMassDecorrelatedDeepHWWV1JetTagsProbs + _pfMassDecorrelatedDeepHWWV1JetTagsMetaDiscrs:
+        supportedBtagDiscr[disc] = [["pfMassDecorrelatedDeepHWWV1TagInfos"]]
+    for disc in _pfMassDecorrelatedInclParticleTransformerV1JetTagsProbs + _pfMassDecorrelatedInclParticleTransformerV1JetTagsMetaDiscrs:
+        supportedBtagDiscr[disc] = [["pfMassDecorrelatedInclParticleTransformerV1TagInfos"]]
+    # update supportedMetaDiscr
+    for disc in _pfMassDecorrelatedDeepHWWV1JetTagsMetaDiscrs:
+        supportedMetaDiscr[disc] = _pfMassDecorrelatedDeepHWWV1JetTagsProbs
+    for disc in _pfMassDecorrelatedInclParticleTransformerV1JetTagsMetaDiscrs:
+        supportedMetaDiscr[disc] = _pfMassDecorrelatedInclParticleTransformerV1JetTagsProbs
+
+    return supportedBtagInfos, supportedBtagDiscr, supportedMetaDiscr
+
+## Import TagInfos additional to RecoBTag_cff
+from PhysicsTools.PFNano.hwwTagger.pfMassDecorrelatedDeepHWWV1_cff import *
+from PhysicsTools.PFNano.hwwTagger.pfMassDecorrelatedInclParticleTransformerV1_cff import *
